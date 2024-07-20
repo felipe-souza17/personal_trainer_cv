1139dfb1a9802ac67fa768bff6b2ca32b4af56c7
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-20 15:36:15 -0300

    feat: exibição do vídeo no streamlit

--- personal_dash.py
+++ personal_dash.py
@@ -6,5 +6,13 @@ from personal_ai import *
 st.set_page_config(layout="wide")
 
 personalAI = PersonalAI("legpress_video.mp4")
+personalAI.run()
 
+placeholder = st.empty()
 st.title("Personal Trainer AI")
+
+while True:
+    frame, results = personalAI.image_q.get()
+
+    with placeholder.container():
+        st.image(frame)
