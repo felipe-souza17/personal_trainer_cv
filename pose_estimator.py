a4ff247d9ecb52ba219bb57399b0d2caab4ded46
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-20 11:54:02 -0300

    feat: le meu video frame por frame, e destroi quando se encerrar

---
+++ pose_estimator.py
@@ -0,0 +1,22 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+
+file_name = "legpress_video.mp4"
+
+cap = cv2.VideoCapture(file_name)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    if ret:
+        cv2.imshow("Frame", frame)
+
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+    else:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
