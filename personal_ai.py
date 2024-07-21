d20f9c96382394e3f95b0d8f3585861b62f588c7
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-20 21:04:46 -0300

    feat: achar o angulo

--- personal_ai.py
+++ personal_ai.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 import threading
 import queue
+import math
 
 BaseOptions = mp.tasks.BaseOptions
 PoseLandmarker = mp.tasks.vision.PoseLandmarker
@@ -13,13 +14,25 @@ PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 class PersonalAI:
-    def __init__(self, file_name="legpress_video.mp4"):
+    def __init__(self, file_name="terra.mp4"):
         self.file_name = file_name
         self.model_path = 'pose_landmarker_heavy.task'
         self.image_q = queue.Queue()
         self.options = PoseLandmarkerOptions(
             base_options=BaseOptions(model_asset_path=self.model_path),
             running_mode=VisionRunningMode.VIDEO)
+        
+    def find_angle(self, landmarks, p1, p2, p3):
+        land = landmarks.pose_landmarks[0]
+        # https://manivannan-ai.medium.com/find-the-angle-between-three-points-from-2d-using-python-348c513e2cd
+        x1, y1 = (land[p1].x, land[p1].y)
+        x2, y2 = (land[p2].x, land[p2].y)
+        x3, y3 = (land[p3].x, land[p3].y)
+
+        angle = math.degrees(math.atan2(y3-y2, x3-x2) - 
+                                math.atan2(y1-y2, x1-x2))
+
+        return angle
 
     def draw_landmarks_on_image(self, rgb_image, detection_result):
         pose_landmarks_list = detection_result.pose_landmarks
@@ -80,7 +93,7 @@ class PersonalAI:
                             break
 
                     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-                    self.image_q.put((frame, detection_result))
+                    self.image_q.put((frame, detection_result, calc_ts))
                 else:
                     break
 
