4a0c19d8f8aac0fa945357727ab0974fe6a22608
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-20 21:24:29 -0300

    feat: exibição no streamlit com lógica implementada para exercicio de levantamento terra

--- personal_dash.py
+++ personal_dash.py
@@ -5,14 +5,37 @@ from personal_ai import *
 
 st.set_page_config(layout="wide")
 
-personalAI = PersonalAI("legpress_video.mp4")
+personalAI = PersonalAI("terra.mp4")
 personalAI.run()
 
-placeholder = st.empty()
+status = "descansando"
+count = 0
 st.title("Personal Trainer AI")
+placeholder = st.empty()
 
 while True:
-    frame, results = personalAI.image_q.get()
+    frame, results,ts = personalAI.image_q.get()
+    if ts == "done": break
+
+    if len(results.pose_landmarks) > 0:
+        hip_angle = personalAI.find_angle(results, 24, 26, 28)
+        elbow_angle = personalAI.find_angle(results, 12, 24, 26)
+
+        # Lógica do terra
+        if hip_angle > 170 and elbow_angle < 60:
+            status = "pronto"
+            dir = "down"
+
+        if status == "pronto":
+            if dir == "down" and elbow_angle > 100:
+                dir = "up"
+                count += 0.5
+            if dir == "up" and elbow_angle < 60:
+                dir = "down"
+                count += 0.5
 
     with placeholder.container():
-        st.image(frame)
+        col1, col2 = st.columns([0.4, 0.6])
+        col1.image(frame)
+        col2.markdown("## **Status:** " + status)
+        col2.markdown(f"### Repetições: {int(count)}")
