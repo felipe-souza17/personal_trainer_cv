074e67b34af7f4c7e22adad6c675eef49aebc101
felipe-souza17 <felipesantos.rsouza@gmail.com>
2024-07-22 22:56:07 -0300

    feat: função para reduzir tamanho do vídeo

---
+++ utils/compress_video.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+def compress_video(input_file, output_file, crf=23):
+    command = [
+        "ffmpeg",
+        "-i", input_file,
+        "-c:v", "libx264",
+        "-crf", str(crf),
+        "-c:a", "copy",
+        output_file
+    ]
+
+    subprocess.run(command)
+
+
+    if os.path.exists(output_file):
+        print(f"Vídeo comprimido com sucesso para {output_file}")
+    else:
+        print(f"Erro ao comprimir o vídeo para {output_file}")
+
